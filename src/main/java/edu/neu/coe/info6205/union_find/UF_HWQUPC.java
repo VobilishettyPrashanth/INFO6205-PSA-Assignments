59702a095a39d512374b0e47eccecbd6f7cefcba
VobilishettyPrashanth <vobilishetty.p@northeastern.edu>
2023-02-19 00:13:45 -0500

    " "

--- src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
@@ -82,7 +82,13 @@ public class UF_HWQUPC implements UF {
         validate(p);
         int root = p;
         // FIXME
-        // END 
+        while(root!=parent[root]) {
+            root=parent[root];
+        };
+        if(pathCompression) {
+            doPathCompression(p);
+        };
+        // END
         return root;
     }
 
@@ -170,7 +176,16 @@ public class UF_HWQUPC implements UF {
 
     private void mergeComponents(int i, int j) {
         // FIXME make shorter root point to taller one
-        // END 
+        if(i==j) return;
+        if(height[i]<height[j]){
+            updateParent(i, j);//parent[i]=j;
+            updateHeight(j, i);//height[j]+=height[i];
+        }
+        else{
+            updateParent(j, i);//parent[j]=i;
+            updateHeight(i, j);//height[i]+=height[j];
+        }
+        // END
     }
 
     /**
@@ -178,6 +193,10 @@ public class UF_HWQUPC implements UF {
      */
     private void doPathCompression(int i) {
         // FIXME update parent to value of grandparent
-        // END 
+        while(i!=parent[i]) {
+            parent[i]=parent[parent[i]];
+            i=parent[i];
+        }
+        // END
     }
 }
